ec86d664c7267f2e486b4cb7228740a88c0924ad
bullfrog <devbullfrog42@gmail.com>
2018-12-22 17:59:14 -0500

    Initial commit of the Azimuthal Equidistant projection transformation functions.

---
+++ +flatmap/+project/azimuthalEquidistant2geo.m
@@ -0,0 +1,29 @@
+function [ lat_deg, lon_deg ] = azimuthalEquidistant2geo( x, y, lat0_deg, lon0_deg, bodyRadius )
+    % Reference: Snyder, John P. (1987). Azimuthal Equidistant projection.
+    % Map Projects: A Working Manual (pp.195-197). U.S. Geological Survey
+    % Professional Paper 1395.
+    % Retrieved from https://pubs.er.usgs.gov/publication/pp1395
+    
+    % Convert degrees to radians
+    lat0 = lat0_deg * pi/180;
+    lon0 = lon0_deg * pi/180;
+    
+    % Calc rho and c per eq. (20-18) & (25-15) on p. 196.
+    rho = hypot(x, y);
+    c = rho / bodyRadius;
+    
+    % Calc lat per eq. (20-14) and set lat to lat0 if rho == 0 per the
+    % paragraph on p.196 between eq. (20-14) & (20-15).
+    lat = asin( cos(c).*sin(lat0) + y.*sin(c).*cos(lat0)./rho );
+    lat(rho == 0) = lat0;
+    
+    % Calc lon per eq. (20-15) and set lon to lon0 if rho == 0 per the
+    % paragraph on p.196 between eq. (20-14) & (20-15).
+    lon = lon0 + atan2( x.*sin(c), rho.*cos(lat0).*cos(c) - y.*sin(lat0).*sin(c) );
+    lon(rho == 0) = lon0;
+    
+    % Convert radians to degrees
+    lat_deg = lat * 180/pi;
+    lon_deg = lon * 180/pi;
+end
+
---
+++ +flatmap/+project/geo2azimuthalEquidistant.m
@@ -0,0 +1,28 @@
+function [ x, y ] = geo2azimuthalEquidistant( lat_deg, lon_deg, lat0_deg, lon0_deg, bodyRadius )
+    % Reference: Snyder, John P. (1987). Azimuthal Equidistant projection.
+    % Map Projects: A Working Manual (pp.195-197). U.S. Geological Survey
+    % Professional Paper 1395.
+    % Retrieved from https://pubs.er.usgs.gov/publication/pp1395
+    
+    % Convert degrees to radians
+    lat = lat_deg * pi/180;
+    lon = lon_deg * pi/180;
+    lat0 = lat0_deg * pi/180;
+    lon0 = lon0_deg * pi/180;
+    
+    % Calc the central angle between the lat, lon point and the center
+    % point (lat0, lon0), per eq. (5-3) & (25-2) on p. 195.
+    cosC = sin(lat0).*sin(lat) + cos(lat0).*cos(lat).*cos(lon-lon0);
+    c = acos( cosC );
+    
+    % Calc the kprime per eq (25-2) and the following paragraph on p. 195.
+    kprime = c ./ sin(c);
+    kprime(cosC == 1) = 1;
+    kprime(cosC == -1) = nan;
+    kprime(abs(cosC) > 1) = nan;
+    
+    % Calc the x & y terms according to eq. (22-4) & (22-5) on p. 195.
+    x = bodyRadius * kprime .* cos(lat) .* sin(lon-lon0);
+    y = bodyRadius * kprime .* ( cos(lat0).*sin(lat) - sin(lat0).*cos(lat).*cos(lon-lon0) );
+end
+
