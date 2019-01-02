dbf397b4b772e3536945ae504edaefd3222e9057
bullfrog <devbullfrog42@gmail.com>
2019-01-02 16:12:10 -0500

    Adjusted flatmap.project.mercator() to correctly use the center longitude parameter (lon0_deg). Now there is a computational geometry problem with drawing polygons.  I'm not yet sure how to fix it.

--- +flatmap/+project/mercator.m
+++ +flatmap/+project/mercator.m
@@ -1,9 +1,12 @@
 function [x, y] = mercator(lat_deg, lon_deg, lon0_deg, bodyRadius)
-    lat  = lat_deg * pi/180;
-    lon  = lon_deg * pi/180;
-    lon0 = lon0_deg * pi/180;
+    lat = lat_deg * pi/180;
+    lon_deg = lon_deg - lon0_deg;
+    lonOutOfBounds = lon_deg < -180 | lon_deg > 180;
+    lon_deg(lonOutOfBounds) = mod(lon_deg(lonOutOfBounds), 360);
+    lon_deg(lon_deg > 180) = lon_deg(lon_deg > 180) - 360;
+    lon = lon_deg * pi/180;
     
-    x = bodyRadius * (lon - lon0);
+    x = bodyRadius * (lon);
     y = bodyRadius * atanh( sin(lat) );
 %     y = bodyRadius * log(tan(pi/4 + lat/2));
 end
