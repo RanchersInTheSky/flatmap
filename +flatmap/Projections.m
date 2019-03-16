a03e208964918ccd9c187d789a3eae4cc55f9bbd
bullfrog <devbullfrog42@gmail.com>
2019-03-16 15:01:14 -0400

    Adding placeholder for boundary handling.

--- +flatmap/Projections.m
+++ +flatmap/Projections.m
@@ -3,22 +3,26 @@ classdef Projections
     enumeration
         mercator ( @flatmap.project.mercator, ...
             @flatmap.project.mercatorInv, ...
+            function_handle.empty(), ...
             {0, 180/pi} )
         azimuthalEquidistant ( @flatmap.project.azimuthalEquidistant, ...
             @flatmap.project.azimuthalEquidistantInv, ...
+            function_handle.empty(), ...
             {90, 0, 180/pi} )
     end
     
     properties
         transform function_handle
         inverseTransform function_handle
+        boundaryTest function_handle
         defaults cell
     end
     
     methods
-        function obj = Projections(transform, inverseTransform, defaults)
+        function obj = Projections(transform, inverseTransform, boundaryTest, defaults)
             obj.transform = transform;
             obj.inverseTransform = inverseTransform;
+            obj.boundaryTest = boundaryTest;
             obj.defaults = defaults;
         end
     end
